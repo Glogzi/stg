78db483b73690831c6999b0c4ea0f205a4cab66f
kmtrebacz <119664516+kmtrebacz@users.noreply.github.com>
2024-03-18 09:09:20 +0100

    style: change the file name

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import time
 import shutil
 
+
 def createMultiple(repeatSentence, title, fileType, num, divide):
     sentenceLines = ""
     sentence = ""
-    
+
     for wordsStatus in range(1, num + 1):
         sentence += repeatSentence
     for linesStatus in range(1, num + 1):
@@ -13,11 +14,12 @@ def createMultiple(repeatSentence, title, fileType, num, divide):
 
     with open(f"{title}.{fileType}", "w", encoding="utf-8") as file:
         file.write(sentenceLines)
-        print(f"created file (1/1)")
+        print("created file (1/1)")
 
     for filesStatus in range(2, round(num/divide) + 1):
         shutil.copy2(f'{title}.{fileType}', f'./{title}{filesStatus}.{fileType}')
         print(f"copied file ({filesStatus}/{round(num/divide)})")
+
 
 if __name__ == "__main__":
     start = time.time()
