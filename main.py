a8a492583869f9860e84720268ed7504867f1d7f
kmtrebacz <119664516+kmtrebacz@users.noreply.github.com>
2024-03-18 09:40:37 +0100

    feat: slpit def createMultiple and add alerts

--- main.py
+++ main.py
@@ -14,28 +14,33 @@ def printError(message):
     print("[\033[91m {} \033[00m] {}" .format("ERR", message))
 
 
-def createMultiple(repeatSentence, title, fileType, num, divide):
+def createFile(repeatSentence, fileName, fileType, num):
     sentenceLines = ""
     sentence = ""
 
+    printWorking("Creating 1st file")
     for wordsStatus in range(1, num + 1):
         sentence += repeatSentence
     for linesStatus in range(1, num + 1):
         sentenceLines += sentence + "\n"
         print(f"created line ({linesStatus}/{num})")
 
-    with open(f"{title}.{fileType}", "w", encoding="utf-8") as file:
+    with open(f"{fileName}.{fileType}", "w", encoding="utf-8") as file:
         file.write(sentenceLines)
-        print("created file (1/1)")
+        printSuccess("Created 1 file")
 
-    for filesStatus in range(2, round(num/divide) + 1):
-        shutil.copy2(f'{title}.{fileType}', f'./{title}{filesStatus}.{fileType}')
-        print(f"copied file ({filesStatus}/{round(num/divide)})")
+
+def copyFiles(fileName, fileType, num):
+    for filesNum in range(2, num+1):
+        printWorking(f"Coping {filesNum} file")
+        shutil.copy2(f'{fileName}.{fileType}', f'./{fileName}{filesNum}.{fileType}')
+        print(f"copied file ({filesNum}/{num})")
+        printSuccess(f"Copied {filesNum} file")
 
 
 if __name__ == "__main__":
     start = time.time()
-    createMultiple("kys ", "kys", "txt", 1000, 2)
+    createFile("kys", "kys", 1000)
     endTime = time.time() - start
     printSuccess(f"task completed in 0.2137, press enter to quit")
     input()
