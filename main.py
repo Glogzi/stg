8c822f3047d12fb0cbac0a429d33b356494c9030
kmtrebacz <119664516+kmtrebacz@users.noreply.github.com>
2024-03-19 10:49:27 +0100

    style: code look better

--- main.py
+++ main.py
@@ -2,47 +2,49 @@ import time
 import shutil
 
 
-def printSuccess(message):
+def print_success(message):
     print("[\033[92m {} \033[00m] {}" .format("OK", message))
 
 
-def printError(message):
+def print_error(message):
     print("[\033[91m {} \033[00m] {}" .format("ERR", message))
 
 
-def copyFiles(fileName, fileType, num):
-    for filesNum in range(2, num+1):
-        shutil.copy2(f'{fileName}.{fileType}', f'./{fileName}{filesNum}.{fileType}')
-        print(f"copied file ({filesNum}/{num})")
-        printSuccess(f"Copied {filesNum} file")
+def copy_files(base_file_name, file_type, num_copies):
+    for file_num in range(2, num_copies + 1):
+        shutil.copy2(f'{base_file_name}.{file_type}', f'./{base_file_name}{file_num}.{file_type}')
+        print(f"Copied file ({file_num}/{num_copies})")
+        print_success(f"Copied {file_num} file")
 
 
-def createFiles(repeatSentence, fileName, fileType, lineNumber, fileNumber):
-    sentenceLines = ""
+def create_files(repeat_sentence, file_name, file_type, num_lines, num_files):
+    sentence_lines = ""
     sentence = ""
 
-    for wordsStatus in range(1, lineNumber + 1):
-        sentence += repeatSentence
-    for linesStatus in range(1, lineNumber + 1):
-        sentenceLines += sentence + "\n"
-        printSuccess(f"Created line ({linesStatus}/{lineNumber})")
+    for word_status in range(1, num_lines + 1):
+        sentence += repeat_sentence
 
-    with open(f"{fileName}.{fileType}", "w", encoding="utf-8") as file:
-        file.write(sentenceLines)
-        printSuccess("Created 1 file")
+    for line_status in range(1, num_lines + 1):
+        sentence_lines += sentence + "\n"
+        print_success(f"Created line ({line_status}/{num_lines})")
 
-    copyFiles(fileName, fileType, fileNumber)
+    with open(f"{file_name}.{file_type}", "w", encoding="utf-8") as file:
+        file.write(sentence_lines)
+        print_success("Created 1 file")
+
+    copy_files(file_name, file_type, num_files)
 
 
 if __name__ == "__main__":
-    print("Walcome to STG!")
-    repeatSentence = input("Please enter sentence to repeat > ")
-    fileName = input("Please enter file name > ")
-    fileType = input("Please enter file type (witout the dot) > ")
-    lineNumber = int(input("Please enter number of lines in one file > "))
-    fileNumber = int(input("Please enter number of files > "))
+    print("Welcome to STG!")
+    repeat_sentence = input("Please enter sentence to repeat > ")
+    file_name = input("Please enter base file name > ")
+    file_type = input("Please enter file type (without the dot) > ")
+    num_lines = int(input("Please enter number of lines in one file > "))
+    num_files = int(input("Please enter number of files > "))
 
-    start = time.time()
-    createFiles(repeatSentence, fileName, fileType, lineNumber, fileNumber)
-    endTime = time.time() - start
-    printSuccess(f"task completed in {endTime}, press enter to quit")
+    start_time = time.time()
+    create_files(repeat_sentence, file_name, file_type, num_lines, num_files)
+    end_time = time.time() - start_time
+    print_success(f"Task completed in {end_time}, press enter to quit")
+
